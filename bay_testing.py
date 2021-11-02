6274e43cc338636de472ad7fcecbaa4cab412073
LeadFreeCandy <39067807+LeadFreeCandy@users.noreply.github.com>
2021-11-01 21:10:16 -0700

    meh

--- bay_testing.py
+++ bay_testing.py
@@ -1,21 +1,109 @@
 from bayes_opt import BayesianOptimization
-import time
-# Bounded region of parameter space
-pbounds = {'x': (0, 10)}
+from bayes_opt import UtilityFunction
+import numpy as np
+import random as rd
+import matplotlib.pyplot as plt
+from matplotlib import gridspec
 
-def black_box_function(x):
-    time.sleep(.25)
-    return 1/x ** 2
+rd.seed(42)
 
-optimizer = BayesianOptimization(
-    f=black_box_function,
-    pbounds=pbounds,
-    random_state=1
-)
 
-optimizer.maximize(
-    init_points=1,
-    n_iter=6,
-)
+values = []
+
+def target(x):
+    out = np.exp(-(x - 2)**2) + np.exp(-(x - 6)**2/10) + 1/ (x**2 + 1) +rd.random() *.1
+
+    # for val in values:
+    #     if -.05 < x-val[0] < .05 and -.05 < out-val[1] < .05:
+    #         out = val[1]
+    #         break
+
+    # values.append([float(x),float(out)])
+
+    return out
+
+
+# def target(x):
+#     return x ** 2
+
+x = np.linspace(-2, 10, 10000).reshape(-1, 1)
+y = target(x)
+
+dist = .2
+
+def target(x):
+    out = np.exp(-(x - 2)**2) + np.exp(-(x - 6)**2/10) + 1/ (x**2 + 1) +rd.random() *.1
+
+    for val in values:
+        if -dist < x-val[0] < dist and -dist < out-val[1] < dist:
+            out = val[1]
+            break
+
+    values.append([x,out])
+
+    return out
+
+
+# plt.plot(x, y);
+
+optimizer = BayesianOptimization(target, {'x': (-2, 10)}, random_state=27, verbose=0)
+optimizer.maximize(init_points=2, n_iter=0, kappa=5)
+
+for i in range(40):
+    optimizer.maximize(init_points=0, n_iter=1, kappa=5)
+    print(list(optimizer.res)[-1])
 
 print(optimizer.max)
+
+def posterior(optimizer, x_obs, y_obs, grid):
+    optimizer._gp.fit(x_obs, y_obs)
+
+    mu, sigma = optimizer._gp.predict(grid, return_std=True)
+    return mu, sigma
+
+def plot_gp(optimizer, x, y):
+    fig = plt.figure(figsize=(16, 10))
+    steps = len(optimizer.space)
+    fig.suptitle(
+        'Gaussian Process and Utility Function After {} Steps'.format(steps),
+        fontdict={'size':30}
+    )
+    
+    gs = gridspec.GridSpec(2, 1, height_ratios=[3, 1]) 
+    axis = plt.subplot(gs[0])
+    acq = plt.subplot(gs[1])
+    
+    x_obs = np.array([[res["params"]["x"]] for res in optimizer.res])
+    y_obs = np.array([res["target"] for res in optimizer.res])
+    
+    mu, sigma = posterior(optimizer, x_obs, y_obs, x)
+    axis.plot(x, y, linewidth=3, label='Target')
+    axis.plot(x_obs.flatten(), y_obs, 'D', markersize=8, label=u'Observations', color='r')
+    axis.plot(x, mu, '--', color='k', label='Prediction')
+
+    axis.fill(np.concatenate([x, x[::-1]]), 
+              np.concatenate([mu - 1.9600 * sigma, (mu + 1.9600 * sigma)[::-1]]),
+        alpha=.6, fc='c', ec='None', label='95% confidence interval')
+    
+    axis.set_xlim((-2, 10))
+    axis.set_ylim((None, None))
+    axis.set_ylabel('f(x)', fontdict={'size':20})
+    axis.set_xlabel('x', fontdict={'size':20})
+    
+    utility_function = UtilityFunction(kind="ucb", kappa=5, xi=0)
+    utility = utility_function.utility(x, optimizer._gp, 0)
+    acq.plot(x, utility, label='Utility Function', color='purple')
+    acq.plot(x[np.argmax(utility)], np.max(utility), '*', markersize=15, 
+             label=u'Next Best Guess', markerfacecolor='gold', markeredgecolor='k', markeredgewidth=1)
+    acq.set_xlim((-2, 10))
+    acq.set_ylim((0, np.max(utility) + 0.5))
+    acq.set_ylabel('Utility', fontdict={'size':20})
+    acq.set_xlabel('x', fontdict={'size':20})
+    
+    axis.legend(loc=2, bbox_to_anchor=(1.01, 1), borderaxespad=0.)
+    acq.legend(loc=2, bbox_to_anchor=(1.01, 1), borderaxespad=0.)
+
+plot_gp(optimizer, x, y)
+
+plt.show()
+
